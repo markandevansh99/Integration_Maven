696ded0f94835cab51e035bdad5eb7eb1f1b5173
markandevansh99 <markandevansh99@gmail.com>
2020-05-02 12:41:36 +0530

    This is the first Commit ( Class1)

---
+++ class1.java
@@ -0,0 +1,11 @@
+
+public class class1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first class. Will be pushing this in Github");
+		// TODO Auto-generated method stub
+
+	}
+
+}
