b9217d6215d4188eb59c41a58d457d8ddf7b379f
markandevansh99 <markandevansh99@gmail.com>
2020-05-02 13:24:35 +0530

    Second Commit

---
+++ src/main/java/class2.java
@@ -0,0 +1,11 @@
+
+public class class2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my second class. Will be pushing this in Github as well.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
